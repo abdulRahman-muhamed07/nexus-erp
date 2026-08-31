af4c6c5721ecc81671c1a7f59aeb18f4c4ef21cf
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 02:56:46 +0300

    refactor: remove duplicated order query abstraction

--- backend/XeoTechErp.Application/Features/Orders/CQRS/IQuery.cs
+++
@@ -1,9 +0,0 @@
-namespace XeoTechErp.Application.CQRS;
-
-public interface IQuery<out TResult> { }
-
-public interface IQueryHandler<in TQuery, TResult>
-    where TQuery : IQuery<TResult>
-{
-    Task<TResult> HandleAsync(TQuery query, CancellationToken cancellationToken = default);
-}
