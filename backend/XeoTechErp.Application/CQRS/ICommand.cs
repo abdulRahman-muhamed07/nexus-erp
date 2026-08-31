e3f17d235601e59c2a16fc459c7549f86206d55a
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-08-31 20:51:18 +0300

    feat: add lightweight cqrs command abstractions

---
+++ backend/XeoTechErp.Application/CQRS/ICommand.cs
@@ -0,0 +1,9 @@
+namespace XeoTechErp.Application.CQRS;
+
+public interface ICommand<out TResult> { }
+
+public interface ICommandHandler<in TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
