39805ce1465d1a5a4d2409cd06db2c999c14953a
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 02:52:40 +0300

    refactor: separate command handler abstraction

---
+++ backend/XeoTechErp.Application/Abstractions/Messaging/ICommandHandler.cs
@@ -0,0 +1,7 @@
+namespace XeoTechErp.Application.Abstractions.Messaging;
+
+public interface ICommandHandler<in TCommand, TResult>
+    where TCommand : ICommand<TResult>
+{
+    Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
