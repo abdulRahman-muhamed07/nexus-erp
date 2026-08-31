4f85b46d1f59e296c571912202c5aadea26ef9e8
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 00:33:17 +0300

    cleanup: remove legacy application abstractions

--- backend/XeoTechErp.Api/Application/Abstractions/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using XeoTechErp.Api.Domain.Entities;
-
-namespace XeoTechErp.Api.Application.Abstractions;
-
-public interface IUserRepository
-{
-    Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
-}
