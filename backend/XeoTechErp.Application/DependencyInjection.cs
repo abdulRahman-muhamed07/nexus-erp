7af5553cceb77ef4f4029ccd77deb874aa341631
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 02:52:22 +0300

    refactor: register finance feature services and order feature contracts

--- backend/XeoTechErp.Application/DependencyInjection.cs
+++ backend/XeoTechErp.Application/DependencyInjection.cs
@@ -1,11 +1,15 @@
 using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
+using XeoTechErp.Application.Abstractions.Messaging;
 using XeoTechErp.Application.Abstractions.Services;
 using XeoTechErp.Application.Common;
-using XeoTechErp.Application.Contracts.Orders;
-using XeoTechErp.Application.CQRS;
-using XeoTechErp.Application.Features.Finance;
+using XeoTechErp.Application.Features.Finance.Assets;
+using XeoTechErp.Application.Features.Finance.Budgets;
+using XeoTechErp.Application.Features.Finance.Dashboard;
+using XeoTechErp.Application.Features.Finance.Expenses;
+using XeoTechErp.Application.Features.Finance.Invoices;
 using XeoTechErp.Application.Features.Orders.Commands.CreateOrder;
+using XeoTechErp.Application.Features.Orders.Common;
 using XeoTechErp.Application.Features.Orders.Queries.GetOrder;
 using XeoTechErp.Application.Features.Orders.Queries.GetOrders;
 using XeoTechErp.Application.Features.Quotes;
@@ -19,12 +23,12 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(ApplicationMappingProfile).Assembly);
+
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<ICustomerService, CustomerService>();
         services.AddScoped<IInventoryService, InventoryService>();
         services.AddScoped<IDashboardService, DashboardService>();
-        services.AddScoped<IFinanceService, FinanceService>();
         services.AddScoped<IQuoteService, QuoteService>();
         services.AddScoped<IActivityService, ActivityService>();
         services.AddScoped<IPaymentService, PaymentService>();
@@ -37,9 +41,16 @@ public static class DependencyInjection
         services.AddScoped<ISettingsService, SettingsService>();
         services.AddScoped<IReportService, ReportService>();
 
+        services.AddScoped<IFinanceReportingService, FinanceReportingService>();
+        services.AddScoped<IAssetService, AssetService>();
+        services.AddScoped<IBudgetService, BudgetService>();
+        services.AddScoped<IExpenseService, ExpenseService>();
+        services.AddScoped<IInvoiceService, InvoiceService>();
+
         services.AddScoped<ICommandHandler<CreateOrderCommand, Result<OrderDto>>, CreateOrderCommandHandler>();
         services.AddScoped<IQueryHandler<GetOrderQuery, OrderDto?>, GetOrderQueryHandler>();
         services.AddScoped<IQueryHandler<GetOrdersQuery, IReadOnlyList<OrderDto>>, GetOrdersQueryHandler>();
+
         return services;
     }
 }
