a36ebddd11c6c28afa86e8a7cdf16cc755a4d76b
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:38:42 +0300

    refactor: remove duplicate service contract

--- backend/XeoTechErp.Application/Services/IActivityService.cs
+++
@@ -1,9 +0,0 @@
-using XeoTechErp.Application.Contracts.Activities;
-
-namespace XeoTechErp.Application.Services;
-
-public interface IActivityService
-{
-    Task<IReadOnlyList<ActivityResponse>> GetRecentAsync(int take = 100, CancellationToken cancellationToken = default);
-    Task<ActivityResponse> CreateAsync(CreateActivityRequest request, CancellationToken cancellationToken = default);
-}
