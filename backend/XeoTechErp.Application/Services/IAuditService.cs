be1b234e5431c812aece0e36623bd64de87d667a
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:38:47 +0300

    refactor: remove duplicate service contract

--- backend/XeoTechErp.Application/Services/IAuditService.cs
+++
@@ -1,9 +0,0 @@
-using XeoTechErp.Application.Contracts.Audit;
-using XeoTechErp.Application.Common;
-
-namespace XeoTechErp.Application.Services;
-
-public interface IAuditService
-{
-    Task<PagedResult<AuditLogResponse>> GetAsync(int page, int pageSize, string? module, CancellationToken cancellationToken = default);
-}
