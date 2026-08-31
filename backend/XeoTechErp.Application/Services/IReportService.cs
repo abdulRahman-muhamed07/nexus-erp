8f039a0dd40a294c745a82dd733b9eb877f4eeaa
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 01:39:09 +0300

    refactor: remove duplicate service contract

--- backend/XeoTechErp.Application/Services/IReportService.cs
+++
@@ -1,8 +0,0 @@
-using XeoTechErp.Application.Contracts.Reports;
-
-namespace XeoTechErp.Application.Services;
-
-public interface IReportService
-{
-    Task<SalesSummaryResponse> GetSalesSummaryAsync(CancellationToken cancellationToken = default);
-}
