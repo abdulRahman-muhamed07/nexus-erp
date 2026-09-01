cd827613f6917c2f081c04ceea643671f384d4da
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-01 19:10:56 +0300

    fix: align auth interface contract namespace

--- src/XeoTechErp.Application/Features/Auth/IAuthService.cs
+++ src/XeoTechErp.Application/Features/Auth/IAuthService.cs
@@ -1,5 +1,5 @@
 using XeoTechErp.Application.Common;
-using XeoTechErp.Application.Features.Auth.Contracts;
+using XeoTechErp.Application.Contracts.Auth;
 
 namespace XeoTechErp.Application.Features.Auth;
 
